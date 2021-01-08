bdfc2f61b763eac9f4f614d7c2762a2858a3416e
Kyun Heo <kyun2dot@gmail.com>
2021-01-08 23:53:25 +0900

    fix: morgan 이름 수정

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('morgan');
+const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 
