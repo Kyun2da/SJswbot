a27dfadc97cfc2f21cfecd465d6721aaecd1fc1f
Kyun Heo <kyun2dot@gmail.com>
2021-01-10 00:09:49 +0900

    feat: ejs, express-session, helmet, hpp, morgan, mysql2, nodemon, redis, sequelize, sequelize-cli 기본 설정

--- app.js
+++ app.js
@@ -2,25 +2,85 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
-const app = express();
+const { sequelize } = require('./Models');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const session = require('express-session');
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
 
 dotenv.config();
+const redisClient = redis.createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASSWORD,
+});
 
-const port = process.env.PORT || 3000;
+const app = express();
 
+app.set('port', process.env.PORT || 8001);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// 데이터베이스 연결
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.log(error);
+  });
+
+// morgan 설정
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
-  // app.use(helmet());
-  // app.use(hpp());
+  app.use(helmet());
+  app.use(hpp());
 } else {
   app.use(morgan('dev'));
 }
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
 app.use(cors({ origin: true, credentials: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+const sessionOption = {
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  store: new RedisStore({ client: redisClient }),
+};
+if (process.env.NODE_ENV === 'production') {
+  sessionOption.proxy = true;
+  // sessionOption.cookie.secure = true;
+}
+app.use(session(sessionOption));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(process.env.PORT);
-  console.log(`현재 포트 http://localhost:${port}에서 구동중입니다..`);
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
 });
