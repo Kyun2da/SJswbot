826dbc3ed142b33bd9d6f33b9459dd7f9596c493
Kyun Heo <kyun2dot@gmail.com>
2021-01-11 23:27:31 +0900

    test: helmet 지우고 swagger 테스트

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ sequelize
 // morgan 설정
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
-  app.use(helmet());
+  //app.use(helmet());
   app.use(hpp());
 } else {
   app.use(morgan('dev'));
