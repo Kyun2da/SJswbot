d93cbaa310b50e9762306854439177cde5c4ad03
Kyun Heo <kyun2dot@gmail.com>
2021-01-13 20:43:48 +0900

    fix : app연결안되던 부분 수정

--- server.js
+++ server.js
@@ -3,23 +3,16 @@ const http2 = require('http2');
 const fs = require('fs');
 const prod = process.env.NODE_ENV === 'production';
 
+const options = {
+  ca: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/fullchain.pem'),
+  key: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/cert.pem'),
+};
+
 if (prod) {
-  http2
-    .createSecureServer(
-      {
-        ca: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/fullchain.pem'),
-        key: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/privkey.pem'),
-        cert: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/cert.pem'),
-      },
-      (req, res) => {
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        res.write('<h1>Hello Node</h1>');
-        res.end('<p>Hello Server!</p>');
-      },
-    )
-    .listen(443, () => {
-      console.log('443번 포트에서 대기중입니다.');
-    });
+  http2.createSecureServer(options, app).listen(443, () => {
+    console.log('443번 포트에서 대기중입니다.');
+  });
 } else {
   app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
