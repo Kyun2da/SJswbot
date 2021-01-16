deb7014adee6f0895dafd31f1ea1ca5e9a3fe6d4
Kyun Heo <kyun2dot@gmail.com>
2021-01-17 01:11:28 +0900

    fix: dev버전에서는 못읽어오므로 prod 안에 넣기

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const https = require('https');
 const fs = require('fs');
 const prod = process.env.NODE_ENV === 'production';
 
-const options = {
-  ca: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/fullchain.pem'),
-  key: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/cert.pem'),
-};
-
 if (prod) {
+  const options = {
+    ca: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/fullchain.pem'),
+    key: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/sjswbot.site/cert.pem'),
+  };
   https.createServer(options, app).listen(443, () => {
     console.log('443번 포트에서 대기중입니다.');
   });
