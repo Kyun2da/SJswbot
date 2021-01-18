716981e35eac422c7bbd80c4cfcb0140479b8675
Kyun Heo <kyun2dot@gmail.com>
2021-01-18 20:06:16 +0900

    feat: department swagger 추가

--- controllers/department.controller.js
+++ controllers/department.controller.js
@@ -6,6 +6,7 @@ const getDepartment = async (req, res, next) => {
     console.log(users);
     return res.status(200).json({
       success: true,
+      message: '성공적으로 학과를 가져왔습니다.',
       result: users,
     });
   } catch (err) {
