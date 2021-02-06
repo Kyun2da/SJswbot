7bc70655d1917a74b421b7c5b262cbfa94e15888
Kyun Heo <kyun2dot@gmail.com>
2021-02-06 21:43:50 +0900

    fix: ignore_watch remove

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,6 @@ module.exports = {
       script: './server.js',
       autorestart: true,
       watch: true,
-      ignore_watch: ['uploads'],
-      watch_options: {
-        followSymlinks: false,
-      },
       instances: 0,
       exec_mode: 'cluster',
       env: {
